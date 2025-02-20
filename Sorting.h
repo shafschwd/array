aa8c5afb272f01dc2048ff5109680b60830e146d
ahhuangggg <166536093+ahhuangggg@users.noreply.github.com>
2025-02-20 20:09:46 +0800

    Update Sorting.h

--- Sorting.h
+++ Sorting.h
@@ -1,11 +1,27 @@
-﻿#ifndef SORTING_H
+#ifndef SORTING_H
 #define SORTING_H
 
 #include "NewsArticle.h"
 
-void quickSort(NewsArticle arr[], int low, int high);
-void measureSortingTime(NewsArticle arr[], int size);
-int partition(NewsArticle arr[], int low, int high);
+/**
+ * @brief Recursively performs Merge Sort on an array of NewsArticle.
+ */
+void mergeSort(NewsArticle arr[], int left, int right);
+
+/**
+ * @brief Performs Bubble Sort on an array of NewsArticle.
+ */
+void bubbleSort(NewsArticle arr[], int n);
+
+/**
+ * @brief Sorts articles by year (ascending) using Merge Sort and displays them.
+ */
+void sortArticlesByYear(NewsArticle articles[], int articleCount);
+
+/**
+ * @brief Compares Merge Sort vs Bubble Sort for performance.
+ */
+void compareSortingAlgorithms(NewsArticle articles[], int articleCount);
 
 #endif
 
