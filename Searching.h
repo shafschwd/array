d9dc35d6b8bb1ec0a4968d867629144e5950eeec
ahhuangggg <166536093+ahhuangggg@users.noreply.github.com>
2025-02-20 20:10:00 +0800

    Update Searching.h

--- Searching.h
+++ Searching.h
@@ -2,9 +2,16 @@
 #define SEARCHING_H
 
 #include "NewsArticle.h"
+#include <string>
 
+/**
+ * @brief Searches for articles containing a given keyword in the title or text.
+ *
+ * @param arr       Array of NewsArticle
+ * @param size      Number of articles
+ * @param keyword   Keyword to search for
+ */
 void searchKeyword(NewsArticle arr[], int size, const std::string& keyword);
 
 #endif
-
 
